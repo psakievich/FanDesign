4383ae80db393dfc56fe0e1bd3e115f90ce4a39e
psakievich <psakievich@gmail.com>
2016-09-12 18:51:43 -0700

    Add RunCl to XFOIL.py

--- XFOIL.py
+++ XFOIL.py
@@ -4,7 +4,6 @@ Created on Mon Aug 29 12:44:44 2016
 
 @author: psakievich
 """
-import numpy as np
 import subprocess as sp
 
 #Class for running XFOIL in Python
@@ -12,9 +11,12 @@ class XFOIL:
     #set default constructor
     def __init__(self):
         self.outfile='default'
+        self.fileExtension='.txt'
+        self.infile=None
+        self.fromFile=False
         self.xFoilPath='/Applications/Xfoil.app/Contents/Resources/xfoil'
         self.reynolds=5000.0
-        self.NACA=6512
+        self.naca=6512
         self.iterations=70
         self.mach=0.1
         self.verbose=False
@@ -26,19 +28,44 @@ class XFOIL:
             #ps.wait()
             if (self.verbose):
                 print(cmd)
-        StdCmd('NACA '+str(self.NACA))
+        StdCmd('NACA '+str(self.naca))
         StdCmd('OPER')
         StdCmd('ITER '+str(self.iterations))
         StdCmd('VISC '+str(self.reynolds))
         StdCmd('RE '+str(self.reynolds))
         StdCmd('MACH '+str(self.mach))
         StdCmd('PACC')
-        StdCmd(self.outfile)
+        StdCmd(self.outfile+self.fileExtension)
         StdCmd(' ')
         if(seq):
             StdCmd('Aseq '+str(Amin)+' '+str(Amax)+' '+str(Ainc))
         else:
             StdCmd('Alfa '+str(Amin))
+        StdCmd('PACC')
+        StdCmd(' ')
+        StdCmd('QUIT ')
+        ps.communicate()
+    def RunCl(self,Cmin,Cmax=0,Cinc=0,seq=False):
+        ps=sp.Popen([self.xFoilPath],stdin=sp.PIPE,stdout=None,stderr=None,
+                universal_newlines=True)
+        def StdCmd(cmd):
+            ps.stdin.write(cmd+'\n')
+            #ps.wait()
+            if (self.verbose):
+                print(cmd)
+        StdCmd('NACA '+str(self.naca))
+        StdCmd('OPER')
+        StdCmd('ITER '+str(self.iterations))
+        StdCmd('VISC '+str(self.reynolds))
+        StdCmd('RE '+str(self.reynolds))
+        StdCmd('MACH '+str(self.mach))
+        StdCmd('PACC')
+        StdCmd(self.outfile+self.fileExtension)
+        StdCmd(' ')
+        if(seq):
+            StdCmd('Cseq '+str(Cmin)+' '+str(Cmax)+' '+str(Cinc))
+        else:
+            StdCmd('Cl '+str(Cmin))
         StdCmd('PACC')
         StdCmd(' ')
         StdCmd('QUIT ')
